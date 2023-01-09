ba47ce0a4622c69672601e759403036f6aaa4b30
bipblop555 <tom.bonnet@hotmail.com>
2023-01-09 23:33:41 +0100

    abstract/securityController + database.php

---
+++ app/Controller/AbstractController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+
+abstract class AbstractController
+{
+    
+    public function __construct(string $action, array $params = [])
+    {
+        if (!is_callable([$this, $action])) {
+           throw new \RuntimeException("La methode $action n'est pas disponible dans ce controller");
+        }
+        call_user_func_array([$this, $action], $params);
+
+    }
+
+    public function render(string $view, array $args = [], string $title = "Document")
+    {
+        $view = dirname(__DIR__, 2) . '/views/' . $view;
+        $base = dirname(__DIR__, 2) . '/views/base.php';
+
+        ob_start();
+        foreach ($args as $key => $value) {
+            ${$key} = $value;
+        }
+
+        unset($args);
+
+        require_once $view;
+        $_pageContent = ob_get_clean();
+        $_pageTitle = $title;
+
+
+        require_once $base;
+
+        exit;
+    }
+
+    public function getUser() : ?User {
+        return isset($_SESSION['user']) ? unserialize($_SESSION['user']) : null;
+    }
+
+}
+
