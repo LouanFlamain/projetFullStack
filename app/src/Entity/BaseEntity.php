50d456e94f6a9d1cff65ec9f07d16c31f7f3f369
Rayane <mabroukr1997@gmail.com>
2023-01-10 11:44:08 +0100

    add relationship logic

--- app/src/Entity/BaseEntity.php
+++ app/src/Entity/BaseEntity.php
@@ -10,16 +10,58 @@ abstract class BaseEntity
 {
     use Hydrator;
 
-    private ?object $relationship;
+    private object|string|null $relationship;
 
     public function __construct(array $data = [])
     {
         $this->hydrate($data);
     }
 
+    /**
+     * @return object|null
+     */
+    public function getRelationship(): ?object
+    {
+        return $this->relationship;
+    }
+
+    /**
+     * @param object|null $relationship
+     * @return $this
+     */
+    public function setRelationship(?object $relationship)
+    {
+        $this->relationship = $relationship;
+        return $this;
+    }
+
     public function belongTo($data)
     {
-        $this->relationship = (new $data(new PDOFactory()))->getById($this->getId());
+        $relation = (new $data(new PDOFactory()))->getById($this->getId());
+
+        if($relation == new \stdClass()){
+            $this->relationship = $data;
+        } else {
+            $this->relationship = $relation;
+        }
+
+        return $this;
+    }
+
+    public function hasMany($data)
+    {
+        $this->relationship = (new $data(new PDOFactory()))->getById(...$this->getId());
+        return $this;
+    }
+
+    public function save()
+    {
+        $obj = new $this->relationship(new PDOFactory());
+        if(is_callable($obj, 'insertById')){
+            $this->relationship = $obj->insertById($this->getId());
+        } else {
+            $this->relationship = $obj->insert($this->getId());
+        }
         return $this;
     }
 }
