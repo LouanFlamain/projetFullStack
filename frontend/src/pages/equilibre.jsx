2f0cac7672a92b5ff6e082130fbb129747c2ce84
Hassan Aidibe <101555873+HascoVice@users.noreply.github.com>
2023-01-11 12:14:14 +0100

    commit 2

--- frontend/src/pages/equilibre.jsx
+++ frontend/src/pages/equilibre.jsx
@@ -2,6 +2,6 @@ import React from "react";
 
 export default function Equilibre() {
   return (
-    <div><p>ceci est la page equilibre</p></div>
+    <div><p>ceci est la page tototoo</p></div>
     );
   }
