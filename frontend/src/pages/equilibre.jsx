6e360eef62038a3872ab52828287c291fee93f77
Hassan Aidibe <101555873+HascoVice@users.noreply.github.com>
2023-01-11 14:23:03 +0100

    Form rental v1

--- frontend/src/pages/equilibre.jsx
+++ frontend/src/pages/equilibre.jsx
@@ -2,6 +2,44 @@ import React from "react";
 
 export default function Equilibre() {
   return (
-    <div><p>ceci est la page tototoo</p></div>
+    <>
+    <div><p>ceci est la page cocococo</p></div>
+    <div>
+
+      <p>Creer des comptes (Etape 1 sur 2)</p>
+      <div>
+
+        <span>
+
+          <p>
+            Choisissez un titre explicite et donnez plus d'informations dans la description
+          </p>
+          
+        </span>
+
+      </div>
+      <form method="POST">
+
+        <div>
+
+          <label for="title">Titre:</label>
+
+          <input type="text" name="title"/>
+
+        </div>
+
+        <div>
+
+          <label for="currency">Devise:</label>
+
+          <input type="text" name="currency" placeholder="EUR"/> <p>(EUR,USD,CHF,...)</p>
+
+        </div>
+
+      </form>
+
+    
+    </div>
+    </>
     );
   }
