edad2bb1754003b537ceb32cfb2895457cb20264
Hassan Aidibe <101555873+HascoVice@users.noreply.github.com>
2023-01-11 12:11:14 +0100

    commit

--- frontend/src/pages/equilibre.jsx
+++ frontend/src/pages/equilibre.jsx
@@ -2,6 +2,6 @@ import React from "react";
 
 export default function Equilibre() {
   return (
-    <div><p>ceci est la page rental</p></div>
+    <div><p>ceci est la page equilibre</p></div>
     );
   }
