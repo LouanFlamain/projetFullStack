375526d9da09def3c6682fbf580c0a93e0133973
bipblop555 <tom.bonnet@hotmail.com>
2023-01-14 16:56:17 +0100

    true false value expired token

--- app/src/Helpers/JWTHelper.php
+++ app/src/Helpers/JWTHelper.php
@@ -25,14 +25,19 @@ class JWTHelper
     {
         try 
         {
+            $expired = false;
+            echo json_encode([
+                "expired" => $expired
+            ]);
             return JWT::decode($jwt, new Key("je_suis_presque_impossible_a_casser_:3", "HS256"));
         } 
         catch (\Exception $exception) 
         {
             if($exception->getMessage() == "Expired token")
             {
+                $expired = true;
                 echo json_encode([
-                    "message" => "Token expiré"
+                    "expired" => $expired
                 ]);
                 die;  
             }
